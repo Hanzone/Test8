8a17f403b7acf3796a5b78268b379569114da85a
Hanzone <hanzone.hao@ximalaya.com>
2018-04-12 21:26:37 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+
+/**
+ * Created by Hanzone on 2018/4/12.
+ */
+public class Test {
+
+    public static void main(String args[]) {
+        out.print("Hello, Java8");
+    }
+
+}
